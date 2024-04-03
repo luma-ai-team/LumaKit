057c41eeecb0614bfd4f0fffaaadb182d848d92a
Anton Kormakov <anton.kormakov@rosberry.com>
2024-04-03 12:01:10 +0600

    Add UIFont.Width wrapper

--- Sources/LumaKit/Core/Extensions/UIKit/UIFont+Convenience.swift
+++ Sources/LumaKit/Core/Extensions/UIKit/UIFont+Convenience.swift
@@ -5,6 +5,30 @@
 import UIKit
 
 public extension UIFont {
+    enum CompatibleWidth {
+        case standard
+        case condensed
+        case expanded
+        case compressed
+
+        var systemWidth: Width? {
+            if #available(iOS 16.0, *) {
+                switch self {
+                case .standard:
+                    return .standard
+                case .condensed:
+                    return .condensed
+                case .expanded:
+                    return .expanded
+                case .compressed:
+                    return .compressed
+                }
+            }
+            
+            return nil
+        }
+    }
+
     static func roundedSystemFont(ofSize size: CGFloat, weight: Weight) -> UIFont {
         let font: UIFont = .systemFont(ofSize: size, weight: weight)
         guard let descriptor = font.fontDescriptor.withDesign(.rounded) else {
@@ -14,9 +38,9 @@ public extension UIFont {
         return .init(descriptor: descriptor, size: font.pointSize)
     }
 
-    static func compatibleSystemFont(ofSize size: CGFloat, weight: Weight, width: Width) -> UIFont {
+    static func compatibleSystemFont(ofSize size: CGFloat, weight: Weight, width: CompatibleWidth) -> UIFont {
         if #available(iOS 16.0, *) {
-            return .systemFont(ofSize: size, weight: weight, width: width)
+            return .systemFont(ofSize: size, weight: weight, width: width.systemWidth ?? .standard)
         } else {
             return .systemFont(ofSize: size, weight: weight)
         }
