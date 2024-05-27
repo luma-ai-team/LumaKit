25ad2b6d2b91632182a7f148f3ee4be8c6be756b
Anton Kormakov <anton.kormakov@rosberry.com>
2024-05-27 15:29:01 +0600

    Rename setAutolaytouSize func

--- Sources/LumaKit/Core/Extensions/UIKit/UIView+Constraints.swift
+++ Sources/LumaKit/Core/Extensions/UIKit/UIView+Constraints.swift
@@ -27,7 +27,7 @@ public extension UIView {
         centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
     
-    func setAutolayoutSize(size: CGSize) {
+    func activateSizeAnchors(with size: CGSize) {
         translatesAutoresizingMaskIntoConstraints = false
         widthAnchor.constraint(equalToConstant: size.width).isActive = true
         heightAnchor.constraint(equalToConstant: size.height).isActive = true
