62bbb6dc0d44ce2278e24af5a221a2e1e0c120e9
Anton Kormakov <anton.kormakov@rosberry.com>
2024-08-20 12:27:03 +0600

    Fix animated gradient view coder init

--- Sources/LumaKit/Presentation/Components/AnimatedGradientView/AnimatedGradientView.swift
+++ Sources/LumaKit/Presentation/Components/AnimatedGradientView/AnimatedGradientView.swift
@@ -32,7 +32,7 @@ open class AnimatedGradientView: GradientView {
     }
     
     required public init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
     }
     
     open override func didMoveToSuperview() {
