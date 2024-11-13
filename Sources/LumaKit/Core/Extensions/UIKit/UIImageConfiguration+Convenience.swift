ed2605153e477f1dd06813c92f276da9884751d6
Anton Kormakov <anton.kormakov@rosberry.com>
2024-11-13 12:32:53 +0600

    Add UIImage.Configuration convenience extension

---
+++ Sources/LumaKit/Core/Extensions/UIKit/UIImageConfiguration+Convenience.swift
@@ -0,0 +1,22 @@
+//
+//  UIImageConfiguration+Convenience.swift
+//  LumaKit
+//
+//  Created by Anton Kormakov on 13.11.2024.
+//
+
+import UIKit
+
+extension UIImage.Configuration {
+    static func symbol(size: CGFloat,
+                       weight: UIImage.SymbolWeight = .regular,
+                       palette: [UIColor]?) -> UIImage.SymbolConfiguration {
+        var configuration = UIImage.SymbolConfiguration(pointSize: size, weight: weight)
+        if let palette = palette {
+            let colorConfiguration = UIImage.SymbolConfiguration(paletteColors: palette)
+            configuration = configuration.applying(colorConfiguration)
+        }
+
+        return configuration
+    }
+}
