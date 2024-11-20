2c021276434180ec5d2bf3c7d34ef2443a28ae62
Anton Kormakov <anton.kormakov@rosberry.com>
2024-11-20 13:15:56 +0600

    Add Task do-catch-finally convenience func

---
+++ Sources/LumaKit/Core/Extensions/Foundation/Task+Convenience.swift
@@ -0,0 +1,25 @@
+//
+//  Task+Convenience.swift
+//  LumaKit
+//
+//  Created by Anton Kormakov on 20.11.2024.
+//
+
+extension Task {
+
+    @discardableResult
+    public static func `do`(_ operation: @Sendable @escaping () async throws -> Void,
+                            catch errorHandler: @Sendable @escaping (Error) async -> Void,
+                            finally: (@Sendable () async -> Void)?) -> Task<Void, Error> where Success == Void, Failure == Error {
+        return Task {
+            do {
+                try await operation()
+            }
+            catch {
+                await errorHandler(error)
+            }
+
+            await finally?()
+        }
+    }
+}
